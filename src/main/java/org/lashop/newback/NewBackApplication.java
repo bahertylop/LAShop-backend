2eead53641f9577cf83c2ed7a0b63a778d314b64
bahertylop <levichankin@gmail.com>
2024-02-13 22:48:06 +0300

    start project commit

---
+++ src/main/java/org/lashop/newback/NewBackApplication.java
@@ -0,0 +1,13 @@
+package org.lashop.newback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lashop/newback/NewBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lashop.newback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
