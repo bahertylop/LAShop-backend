45b3d05fe2dce5f3be2a47d57f67f1310a8d6efa
bahertylop <levichankin@gmail.com>
2024-02-13 23:13:33 +0300

    модели для категории продуктов и типов обуви

---
+++ src/main/java/org/lashop/newback/models/Category.java
@@ -0,0 +1,28 @@
+package org.lashop.newback.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private long id;
+
+    @Column(name = "category")
+    private String name;
+
+//    @OneToMany(mappedBy = "category")
+//    private List<Product> products;
+}
