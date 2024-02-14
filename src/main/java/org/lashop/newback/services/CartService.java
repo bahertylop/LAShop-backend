840477d39acae9d1f4ab6923dde39e5e3c095893
bahertylop <levichankin@gmail.com>
2024-02-14 17:56:40 +0300

    сделаны CartService и impl

--- src/main/java/org/lashop/newback/services/CartService.java
+++ src/main/java/org/lashop/newback/services/CartService.java
@@ -1,4 +1,22 @@
 package org.lashop.newback.services;
 
+import org.lashop.newback.dto.CartDto;
+import org.lashop.newback.dto.ShoeTypeDto;
+import org.lashop.newback.models.ShoeType;
+import org.lashop.newback.repositories.CartRepository;
+
+import java.util.List;
+
 public interface CartService {
+
+    List<ShoeTypeDto> takeProductsInCart(long accountId);
+
+    List<CartDto> takeCart(long accountId);
+
+    void minusCount(long shoeTypeId, double size, long accountId);
+    void plusCount(long shoeTypeId, double size, long accountId);
+
+    void deleteItemFromCart(long shoeTypeId, double size, long accountId);
+
+    void addToCart(long shoeTypeId, double size, long accountId);
 }
