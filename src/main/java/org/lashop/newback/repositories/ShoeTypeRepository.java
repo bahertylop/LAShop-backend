6250e77e1cd8dee08841063771ed859a0660c861
bahertylop <levichankin@gmail.com>
2024-02-15 11:29:29 +0300

    categoryService and impl commit

--- src/main/java/org/lashop/newback/repositories/ShoeTypeRepository.java
+++ src/main/java/org/lashop/newback/repositories/ShoeTypeRepository.java
@@ -2,8 +2,15 @@ package org.lashop.newback.repositories;
 
 import org.lashop.newback.models.ShoeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 public interface ShoeTypeRepository extends JpaRepository<ShoeType, Long> {
 
+    List<ShoeType> getShoeTypesByCategoryId(long categoryId);
 
+    @Query("SELECT p from ShoeType p where p.category.id = :categoryId")
+    List<ShoeType> getShoeTypesByCategoryIdLimited(long categoryId, Pageable limit);
 }
