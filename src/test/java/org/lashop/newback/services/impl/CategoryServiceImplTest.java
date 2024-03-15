5b69f0859ff9604f1588b9b559b6539780719c84
bahertylop <levichankin@gmail.com>
2024-03-15 16:46:52 +0300

    CategotyServiceImpl test

--- src/test/java/org/lashop/newback/services/impl/CategoryServiceImplTest.java
+++ src/test/java/org/lashop/newback/services/impl/CategoryServiceImplTest.java
@@ -2,17 +2,18 @@ package org.lashop.newback.services.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import jakarta.persistence.EntityNotFoundException;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.lashop.newback.dto.CategoryDto;
+import org.lashop.newback.dto.ProductDto;
 import org.lashop.newback.dto.ShoeTypeDto;
-import org.lashop.newback.models.Account;
-import org.lashop.newback.models.Category;
-import org.lashop.newback.models.Product;
-import org.lashop.newback.models.ShoeType;
+import org.lashop.newback.models.*;
 import org.lashop.newback.repositories.CategoryRepository;
 import org.lashop.newback.repositories.ShoeTypeRepository;
+import org.lashop.newback.services.ShoeTypeService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -20,6 +21,7 @@ import org.springframework.data.domain.Limit;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import static org.mockito.Mockito.*;
@@ -192,7 +194,118 @@ public class CategoryServiceImplTest {
         Assertions.assertEquals(category.getId(), categoryDto.getId());
         Assertions.assertEquals(category.getName(), categoryDto.getName());
         Assertions.assertEquals(category.getImage(), categoryDto.getImage());
+    }
+
+    @Test
+    void testGetCategoryByIdThrowsException() {
+        long categoryId = 1;
+
+        when(categoryRepository.getReferenceById(categoryId)).thenThrow(EntityNotFoundException.class);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            categoryService.getCategoryById(categoryId);
+        });
+    }
+
+    @Test
+    void testGetShoeTypeCategory() {
+        ShoeTypeDto shoeTypeDto = ShoeTypeDto.builder().categoryId(1).id(1).build();
+
+        CategoryDto categoryDto = CategoryDto.builder().id(1).name("shoe").image("image").shoeTypeIds(List.of(1L)).build();
+        when(categoryRepository.getReferenceById(shoeTypeDto.getCategoryId())).thenReturn(Category.builder().id(1).name("shoe").image("image").products(List.of(ShoeType.builder().id(1).build())).build());
+
+        CategoryDto res = categoryService.getShoeTypeCategory(shoeTypeDto);
+
+        Assertions.assertEquals(categoryDto, res);
 
     }
+
+    @Test
+    void testGetProductCategory() {
+        // Подготовка данных
+        long typeId = 1L;
+        long categoryId = 2L;
+
+        ProductDto productDto = ProductDto.builder().typeId(typeId).build();
+
+        ShoeType shoeType = ShoeType.builder().category(Category.builder()
+                .id(categoryId)
+                .name("shoe")
+                .image("image")
+                .build()
+        ).build();
+
+        Category category = Category.builder()
+                .id(categoryId)
+                .name("shoe")
+                .image("image")
+                .products(List.of(shoeType))
+                .build();
+
+        when(shoeTypeRepository.getReferenceById(typeId)).thenReturn(shoeType);
+        when(categoryRepository.getReferenceById(categoryId)).thenReturn(category);
+
+        CategoryDto result = categoryService.getProductCategory(productDto);
+
+        assertNotNull(result);
+        assertEquals(category.getId(), result.getId());
+        assertEquals(category.getImage(), result.getImage());
+        assertEquals(category.getName(), result.getName());
+    }
+
+    @Test
+    void testCreateCategory() {
+        String categoryName = "category";
+        String image = "image";
+
+        when(categoryRepository.findCategoryByName(categoryName)).thenReturn(Optional.empty());
+
+        categoryService.createCategory(categoryName, image);
+
+        verify(categoryRepository, times(1)).save(any(Category.class));
+    }
+
+    @Test
+    void testCreateCategoryThrowsException() {
+        String categoryName = "category";
+        String image = "image";
+
+        when(categoryRepository.findCategoryByName(categoryName)).thenReturn(Optional.of(Category.builder().name(categoryName).image(image).build()));
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            categoryService.createCategory(categoryName, image);
+        });
+    }
+
+    @Test
+    void testDeleteCategory() {
+        Long categoryId = 1L;
+
+        categoryService.deleteCategory(categoryId);
+
+        verify(categoryRepository, times(1)).deleteById(any(Long.class));
+    }
+
+    @Test
+    void testDeleteCategoryAndTakeTypesNotInStock() {
+        String categoryName = "category";
+        long categoryId = 1L;
+
+        Category categoryStub = Category.builder().id(categoryId).name(categoryName).build();
+        when(categoryRepository.findCategoryByName(categoryName)).thenReturn(Optional.of(categoryStub));
+
+        List<ShoeType> shoeTypes = Arrays.asList(
+                ShoeType.builder().id(1L).category(categoryStub).inStock(true).build(),
+                ShoeType.builder().id(2L).category(categoryStub).inStock(true).build()
+        );
+        when(shoeTypeRepository.getShoeTypesByCategoryId(categoryId)).thenReturn(shoeTypes);
+
+        categoryService.deleteCategoryAndTakeTypesNotInStock(categoryName);
+
+        verify(shoeTypeRepository, times(1)).makeAllNotInStockByCategoryId(categoryId);
+        verify(categoryRepository, times(1)).updateAllByCategoryIdToNull(categoryId);
+        verify(categoryRepository, times(1)).deleteById(categoryId);
+    }
+
 
 }
---
+++ src/test/java/org/lashop/newback/services/impl/FavouritesServiceImplTest.java
@@ -0,0 +1,24 @@
+package org.lashop.newback.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FavouritesServiceImplTest {
+
+    @Test
+    void getFavouritesList() {
+    }
+
+    @Test
+    void deleteFavourites() {
+    }
+
+    @Test
+    void addShoeTypeToFavourites() {
+    }
+
+    @Test
+    void deleteFromFavourites() {
+    }
+}
