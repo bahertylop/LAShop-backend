8270872726813f78779fe4cfebb7be7b68b03e25
bahertylop <levichankin@gmail.com>
2024-03-17 17:47:35 +0300

    add and delete favourite work

--- src/main/java/org/lashop/newback/dto/requests/OneFavouriteRequest.java
+++ src/main/java/org/lashop/newback/dto/requests/OneFavouriteRequest.java
@@ -1,10 +1,14 @@
 package org.lashop.newback.dto.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OneFavouriteRequest {
 
     private Long shoeTypeId;
